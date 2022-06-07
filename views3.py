a9ffb7489ebbc913a0574035e2c9b9940c4f8c01
saurabh pagar <98882380+saurabhpagar@users.noreply.github.com>
2022-06-07 12:17:45 +0530

    Create views3.py

---
+++ views3.py
@@ -0,0 +1,18 @@
+def view_issued_book(request):
+    issuedBooks = IssuedBook.objects.all()
+    details = []
+    for i in issuedBooks:
+        days = (date.today()-i.issued_date)
+        d=days.days
+        fine=0
+        if d>14:
+            day=d-14
+            fine=day*5
+        books = list(models.Book.objects.filter(isbn=i.isbn))
+        students = list(models.Student.objects.filter(user=i.student_id))
+        i=0
+        for l in books:
+            t=(students[i].user,students[i].user_id,books[i].name,books[i].isbn,issuedBooks[0].issued_date,issuedBooks[0].expiry_date,fine)
+            i=i+1
+            details.append(t)
+    return render(request, "view_issued_book.html", {'issuedBooks':issuedBooks, 'details':details})
