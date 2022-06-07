8205d4cc1cd68c179dee24c6166a839d717455d1
saurabh pagar <98882380+saurabhpagar@users.noreply.github.com>
2022-06-07 12:22:17 +0530

    Create views4.py

---
+++ views4.py
@@ -0,0 +1,19 @@
+def edit_profile(request):
+    student = Student.objects.get(user=request.user)
+    if request.method == "POST":
+        email = request.POST['email']
+        phone = request.POST['phone']
+        branch = request.POST['branch']
+        classroom = request.POST['classroom']
+        roll_no = request.POST['roll_no']
+ 
+        student.user.email = email
+        student.phone = phone
+        student.branch = branch
+        student.classroom = classroom
+        student.roll_no = roll_no
+        student.user.save()
+        student.save()
+        alert = True
+        return render(request, "edit_profile.html", {'alert':alert})
+    return render(request, "edit_profile.html")
